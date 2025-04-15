bb7b513484991f6d31c145ca15befcd4bcbab516
Sage <sagezheng@outlook.com>
2025-04-15 18:05:06 +0800

    feat: network chart

--- src/mian/cards/DisplayCard/index.jsx
+++ src/mian/cards/DisplayCard/index.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
+import * as echarts from 'echarts/core';
+import {LineChart} from 'echarts/charts';
+import {DatasetComponent, GridComponent} from 'echarts/components';
+import {
+    CanvasRenderer,
+} from 'echarts/renderers';
 import {DisplayIcon} from "../../../icon/index.jsx";
+
+echarts.use(
+    [LineChart, CanvasRenderer, DatasetComponent, GridComponent]
+);
+
+const fpsLevels = [60, 144, 240, 300];
+
+function getDynamicMax(currentMax) {
+    for (let level of fpsLevels) {
+        if (currentMax <= level) return level; // 返回对应KB值
+    }
+    return fpsLevels[fpsLevels.length - 1]; // 超过则取最大值
+}
 
 function DisplayCard(
     {
@@ -10,8 +30,58 @@ function DisplayCard(
         fps
     } = data;
 
+    const [source, setSource] = useState(Array.from({length: 20}, (_, i) => [i, 0]));
+
+    useEffect(() => {
+        const {fps} = data;
+
+        setSource(prev => {
+            const [i] = prev.shift();
+            prev.push([i, fps]);
+            return prev;
+        });
+    }, [data]);
+
+    const option = {
+        grid: {
+            show: false,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+        },
+        dataset: {
+            source: [
+                ['time', 'fps'],
+                ...source
+            ]
+        },
+        xAxis: {
+            type: 'category',
+            axisLabel: {show: false},
+            splitLine: {show: false},
+            axisTick: {show: false},
+        },
+        yAxis: {
+            type: 'value',
+            max: value => getDynamicMax(value.max),
+            axisLabel: {show: false},
+            splitLine: {show: false},
+        },
+        series: [
+            {
+                type: 'line',
+                name: 'fps',
+                itemStyle: {
+                    color: '#1AC4FF'
+                },
+            },
+        ],
+        animation: false
+    };
+
     return (
-        <div className="card display-chip">
+        <div className="card card-display">
             <div className="card-header">
                 <div className="card-icon"><DisplayIcon/></div>
                 <div className="card-title">Display</div>
@@ -20,6 +90,12 @@ function DisplayCard(
                 <div className="big-item">
                     {fps} FPS
                 </div>
+                {/*<ReactEChartsCore*/}
+                {/*    echarts={echarts}*/}
+                {/*    option={option}*/}
+                {/*    notMerge={true}*/}
+                {/*    lazyUpdate={true}*/}
+                {/*/>*/}
             </div>
         </div>
     );
--- src/mian/cards/NetworkCard/index.jsx
+++ src/mian/cards/NetworkCard/index.jsx
@@ -1,5 +1,24 @@
-import React, {useMemo} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
+import * as echarts from 'echarts/core';
+import {BarChart} from 'echarts/charts';
+import {DatasetComponent, GridComponent} from 'echarts/components';
+import {
+    CanvasRenderer,
+} from 'echarts/renderers';
 import {NetworkIcon} from "../../../icon/index.jsx";
+
+echarts.use(
+    [BarChart, CanvasRenderer, DatasetComponent, GridComponent]
+);
+
+const speedLevels = [0.05, 0.5, 3, 8, 15, 70, 120].map(v => v * 1024); // 转换为KB/s单位的阈值
+function getDynamicMax(currentMax) {
+    for (let level of speedLevels) {
+        if (currentMax <= level) return level; // 返回对应KB值
+    }
+    return speedLevels[speedLevels.length - 1]; // 超过则取最大值
+}
 
 function NetworkCard(
     {
@@ -8,12 +27,69 @@ function NetworkCard(
 ) {
     const {download = 0, upload = 0} = data;
 
+    const [source, setSource] = useState(Array.from({length: 20}, (_, i) => [i, 0, 0]))
+
+    useEffect(() => {
+        const {download, upload} = data;
+
+        setSource(prev => {
+            const [i] = prev.shift();
+            prev.push([i, upload, download]);
+            return prev;
+        })
+    }, [data]);
+
     const format = useMemo(() => (val) => {
         if (val >= 1024) {
             return (val / 1024).toFixed(2) + ' Mb/s';
         }
         return val.toFixed(1) + ' Kb/s';
-    }, [])
+    }, []);
+
+    const option = {
+        grid: {
+            show: false,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+        },
+        dataset: {
+            source: [
+                ['time', 'upload', 'download'],
+                ...source
+            ]
+        },
+        xAxis: {
+            type: 'category',
+            axisLabel: {show: false},
+            splitLine: {show: false},
+            axisTick: {show: false},
+        },
+        yAxis: {
+            type: 'value',
+            max: value => getDynamicMax(value.max),
+            axisLabel: {show: false},
+            splitLine: {show: false},
+        },
+        series: [
+            {
+                type: 'bar',
+                name: 'upload',
+                itemStyle: {
+                    color: '#E1E1E2'
+                },
+            },
+            {
+                type: 'bar',
+                name: 'download',
+                itemStyle: {
+                    color: '#1AC4FF'
+                },
+            },
+        ],
+        animation: false
+    };
 
     return (
         <div className="card card-network">
@@ -30,6 +106,12 @@ function NetworkCard(
                     <span>Download</span>
                     <span>{format(download)}</span>
                 </div>
+                <ReactEChartsCore
+                    echarts={echarts}
+                    option={option}
+                    notMerge={true}
+                    lazyUpdate={true}
+                />
             </div>
         </div>
     );
